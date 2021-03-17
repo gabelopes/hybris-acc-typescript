1a134cf5c88487eeb50a173267b63c34dd4a1ec3
gabelopes <gabriellopes.nunes@hotmail.com>
2021-03-17 16:43:48 -0300

    Move source files to root

--- index.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="./acc/ACCModuleLoader.ts" />
-
-export * as ACC from "./acc/ACC";
-export * as Autoload from "./acc/Autoload";
-export * as Predicative from "./acc/Predicative";
