75ee16b1839c6ba324cbd22687b12e3b63f26cee
gabelopes <gabriellopes.nunes@hotmail.com>
2021-03-17 16:14:43 -0300

    Adds index.ts

---
+++ index.ts
@@ -0,0 +1,5 @@
+/// <reference path="./acc/ACCModuleLoader.ts" />
+
+export * as ACC from "./acc/ACC";
+export * as Autoload from "./acc/Autoload";
+export * as Predicative from "./acc/Predicative";
